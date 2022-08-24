dca22a8ac38d85e92b84c2aa1cd02a87c44b3755
Juliana Resplande <julianarsg13@gmail.com>
2022-08-24 18:00:44 -0300

    btree

--- b_tree/btree.py
+++ b_tree/btree.py
@@ -1,25 +1,41 @@
-# TODO:
-# - Testar mais
-# - Nome da ordem
-
-# https://www.geeksforgeeks.org/insert-operation-in-b-tree/
+"""
+https://www.ime.usp.br/~pf/estruturas-de-dados/aulas/B-trees.html
+"""
 
 
 class Btree:
     def __init__(self, order: int):
         self.root = BtreeNode(order)
+        self.height = -1
 
     def insert(self, element: int):
+        if self.height == -1:
+            self.height = 0
+
         self.root.insert(element)
 
         while self.root.parent:  # Houve promoção
             self.root = self.root.parent
+            self.height += 1
+
+    def search(self, element: int):
+        return self.root.search(element)
 
     def __repr__(self) -> str:
         return f"Btree(order={self.order})"
 
     def __str__(self) -> str:
-        return str(self.root)
+        line = [self.root]
+
+        out = ""
+        while line:
+            item = line.pop(0)
+            out += str(item) + "\n"
+
+            for child in item.children:
+                line.append(child)
+
+        return out
 
 
 class BtreeNode:
@@ -33,14 +49,24 @@ class BtreeNode:
         return f"BtreeNode(order={self.order}, keys={self.keys})"
 
     def __str__(self) -> str:
-        nodes = str(self.keys) + "\n"
+        return str(self.keys)
 
-        for child in self.children:
-            nodes += str(child)
+    def search(self, element: int):
+        idx = 0
 
-        return nodes
+        for key in self.keys:
+            if element == key:
+                return key
+            elif element < key:
+                break
+            idx += 1
 
-    def insert(self, element: int, promotion_children: list = []):
+        if self.children:
+            return self.children[idx].search(element)
+        else:
+            return -1
+
+    def insert(self, element: int):
         insert_idx = 0
 
         for key in self.keys:
@@ -48,19 +74,12 @@ class BtreeNode:
                 break
             insert_idx += 1
 
-        if self.children and not promotion_children:
+        if self.children:
             self.children[insert_idx].insert(element)
         else:
             self.keys = self.keys[:insert_idx] + [element] + self.keys[insert_idx:]
 
-            if promotion_children:
-                self.children = (
-                    self.children[:insert_idx]
-                    + promotion_children
-                    + self.children[insert_idx:]
-                )
-
-            if len(self.keys) == self.order - 1:
+            if len(self.keys) == self.order:
                 self.promote()
 
     def promote(self):
@@ -75,8 +94,17 @@ class BtreeNode:
         self.children = self.children[: middle_idx + 1]
 
         if not self.parent:
-            self.parent = right_tree.parent = BtreeNode(self.order)
-            self.parent.keys = [middle_key]
-            self.parent.children = [self, right_tree]
-        else:
-            self.parent.insert(middle_key, [self, right_tree])
+            self.parent = BtreeNode(self.order)
+            self.parent.children = [self]
+
+        right_tree.parent = self.parent
+
+        insert_idx = self.parent.children.index(self)
+        self.parent.keys = (
+            self.parent.keys[:insert_idx] + [middle_key] + self.parent.keys[insert_idx:]
+        )
+        self.parent.children = (
+            self.parent.children[: insert_idx + 1]
+            + [right_tree]
+            + self.parent.children[insert_idx + 1 :]
+        )
--- test.py
+++ test.py
@@ -1,10 +1,28 @@
 from b_tree.btree import Btree
 
-btree = Btree(4)
+btree = Btree(3)
 
-for element in [3, 2, 1, 4]:
+elements = [
+    3,
+    2,
+    1,
+    4,
+    10,
+    11,
+    34,
+    120,
+    128,
+    39,
+    190,
+]
+
+for element in elements:
     btree.insert(element)
-    print('Btree root')
-    print(repr(btree.root))
-    print('Btree')
-    print(btree)
+
+print("btree")
+print(btree)
+
+print(btree.search(39))
+print(btree.search(42))
+
+print(btree.height)
